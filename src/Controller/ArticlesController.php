bb7a21358917dcc521257d8a38aa0404f3f3ea51
mohamed7 <moha1234566044@gmail.com>
2020-12-25 17:29:01 +0200

    adding posts

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -10,6 +10,12 @@ namespace App\Controller;
 
   use App\Entity\Article;
 
+  use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+  use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+  use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 
 class ArticlesController extends Controller{
@@ -30,6 +36,54 @@ class ArticlesController extends Controller{
 		return $this->render('articles/articles.html.twig', array('articles' => $articles));
 	}
 
+
+	/**
+	 *@Route("/article/new", name="create.new")
+	 *
+	 *@method({"Get","Post"})
+	 */
+
+
+	public function new(Request $request) {
+
+		$article = new Article();
+
+		$form = $this->createFormBuilder($article)
+		 ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+		 ->add('body', TextareaType::class, array('required' => false,
+		 	'attr' => array('class' => 'form-control')
+		))
+
+		 ->add('submit', SubmitType::class, array('label' => 'create',
+
+		 	'attr' => array('class', 'btn btn-primary
+		 	    mt-3'))
+
+		)
+		 ->getForm();
+
+		 $form->handleRequest($request);
+
+		 if($form->isSubmitted() && $form->isValid()) {
+
+		 	$article = $form->getData();
+
+		 	$entityManager = $this->getDoctrine()->getManager();
+
+		 	$entityManager->persist($article);
+		 	$entityManager->flush();
+
+		 	return $this->redirectToRoute('home');
+
+
+
+		 }
+
+		 return $this->render('articles/new.html.twig', array('form' => $form->createView()));
+
+
+
+	}
 
 	/**
 	 *@Route("/article/{id}", name="show")
